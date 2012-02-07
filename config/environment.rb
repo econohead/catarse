93880cdbd9f1b74653c4c4838acb16f81884c799
Mat Johnson <matjohnson@gmail.com>
2012-02-06 23:05:36 -0800

    load certificates

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Catarse::Application.initialize!
 
+# load certificates
+  require "openid/fetchers"
+  OpenID.fetcher.ca_file = "#{Rails.root}/config/ca-bundle.crt"
