ed76826c80ab48a0270da91c65f5ff24728dfb33
Mat Johnson <matjohnson@gmail.com>
2012-02-08 13:52:28 -0800

    update SendGrid settings to ENV

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -3,8 +3,8 @@ begin
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => 'app2800114@heroku.com',
-  :password       =>  Configuration.find_by_name('sendgrid').value,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
   :domain         => 'heroku.com'
   }
   ActionMailer::Base.delivery_method = :smtp
