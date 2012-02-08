a85fcf1389dc935e219cacff0d0d4fd1b6b7b598
Mat Johnson <matjohnson@gmail.com>
2012-02-08 12:51:56 -0800

    Update email settings

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -3,7 +3,7 @@ begin
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => 'catarse',
+  :user_name      => 'app2800114@heroku.com',
   :password       =>  Configuration.find_by_name('sendgrid').value,
   :domain         => 'heroku.com'
   }
