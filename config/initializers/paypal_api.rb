3172e72de50bf17eff89b2f0f86148d3d8c9d28f
Mat Johnson <matjohnson@gmail.com>
2012-02-15 08:26:41 -0800

    rework paypal_api - made bak

--- config/initializers/paypal_api.rb
+++ config/initializers/paypal_api.rb
@@ -1,10 +1,14 @@
-begin
-  unless Rails.env == 'test'
-    PaypalApi.configure do |config|
-      config.username   = Configuration.find_by_name('paypal_username').value
-      config.password   = Configuration.find_by_name('paypal_password').value
-      config.signature  = Configuration.find_by_name('paypal_signature').value
-    end
-  end
-rescue
+require 'paypal/express'
+
+PAYPAL_CONFIG = if ENV['paypal_username'] 
+  {
+    username:  ENV['paypal_username'],
+    password:  ENV['paypal_password'],
+    signature: ENV['paypal_signature'],
+    sandbox:   ENV['paypal_sandbox']
+  }
+else
+  YAML.load_file("#{Rails.root}/config/paypal.yml")[Rails.env].symbolize_keys
 end
+
+Paypal.sandbox! if PAYPAL_CONFIG[:sandbox]
