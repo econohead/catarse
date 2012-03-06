14012a03dcb536af9286ccedb22634336fb77c13
Mat Johnson <matjohnson@gmail.com>
2012-03-05 16:14:31 -0800

    change to 'en'

--- db/migrate/20110424054103_add_locale_to_users.rb
+++ db/migrate/20110424054103_add_locale_to_users.rb
@@ -1,6 +1,6 @@
 class AddLocaleToUsers < ActiveRecord::Migration
   def self.up
-    add_column :users, :locale, :text, :null => false, :default => 'pt'
+    add_column :users, :locale, :text, :null => false, :default => 'en'
   end
 
   def self.down
