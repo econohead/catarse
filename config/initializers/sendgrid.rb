3baa4bc975d83f35cba8e385cdb1d89524da639d
Mat Johnson <matjohnson@gmail.com>
2012-03-09 06:06:54 -0800

    Re-added sendgrid creeds after adding info to configurations.

---
+++ config/initializers/sendgrid.rb
@@ -0,0 +1,13 @@
+begin
+    ActionMailer::Base.smtp_settings = {
+        :address        => 'smtp.sendgrid.net',
+        :port           => '587',
+        :authentication => :plain,
+        :user_name      => 'app2800114@heroku.com',
+        :password       =>  Configuration.find_by_name('sendgrid').value,
+        :domain         => 'heroku.com'
+    }
+    ActionMailer::Base.delivery_method = :smtp
+    rescue
+    nil
+end
